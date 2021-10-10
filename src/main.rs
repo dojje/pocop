da5fde0506d86ede829ab07af2fba2fff77584fb
dojje <dojje@users.noreply.github.com>
2021-10-10 11:00:19 +0200

    added automatic reseting of target

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // #![windows_subsystem = "windows"]
 
-use bevy::{input::mouse::MouseMotion, math::Vec2Swizzles, prelude::*};
-
+use bevy::prelude::*;
+use rand::Rng;
 
 struct TargetsOnScreen(u8);
 
@@ -11,43 +11,37 @@ struct Gravity(f32);
 
 pub struct Materials {
     _target: Handle<ColorMaterial>,
-
 }
 
-fn setup(
-    mut commands: Commands,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
-}
-
-fn cursor_events(
-    mut cursor_evr: EventReader<CursorMoved>,
-) {
-    for ev in cursor_evr.iter() {
-        println!(
-            "New cursor position: X: {}, Y: {}, in Window ID: {:?}",
-            ev.position.x, ev.position.y, ev.id
-        );
-    }
 }
 
 fn spawn_target(
     mut targets_on_screen: ResMut<TargetsOnScreen>,
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    windows: Res<Windows>,
 ) {
     if targets_on_screen.0 <= 0 {
+        let mut rng = rand::thread_rng();
+        let window = windows.get_primary().unwrap();
+
+        let target_width = window.width() / 10.0;
+
+        let target_x = rng.gen_range(-window.width() / 2.0 + target_width / 2.0 .. window.width() / 2.0 - target_width / 2.0);
+        let target_y = window.height() / 2.0 - target_width / 2.0;
+
         commands
             .spawn_bundle(SpriteBundle {
-            material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            sprite: Sprite::new(Vec2::new(120.0, 120.0)),
-            ..Default::default()
-        })
-        .insert(Target)
-        .insert(Speed(0.0));
-        
+                material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+                transform: Transform::from_xyz(target_x, target_y, 0.0),
+                sprite: Sprite::new(Vec2::new(target_width, target_width)),
+                ..Default::default()
+            })
+            .insert(Target)
+            .insert(Speed(0.0));
+
         targets_on_screen.0 += 1;
     }
 }
@@ -55,34 +49,47 @@ fn spawn_target(
 fn target_click(
     btn: Res<Input<MouseButton>>,
     windows: Res<Windows>,
-    mut query: Query<(&Transform, &Sprite, With<Target>)>
-    
+    mut query: Query<(&Transform, &Sprite, Entity), With<Target>>,
+    mut commands: Commands,
+    mut targets_on_screen: ResMut<TargetsOnScreen>,
+    mut gravity: ResMut<Gravity>,
 ) {
     if btn.just_pressed(MouseButton::Left) {
         let window = windows.get_primary().unwrap();
 
         if let Some(mouse_pos) = window.cursor_position() {
-
-            for (tf, sprite ,_) in query.iter_mut() {
-                println!("target x: {}, target y: {}", tf.translation.x + window.width() / 2.0 - sprite.size.x / 2.0, tf.translation.y + window.height() / 2.0 - sprite.size.y / 2.0);
-                println!("mouse  x: {}, mouse  y: {}", mouse_pos.x, window.height() - mouse_pos.y);
+            for (tf, sprite, entity) in query.iter_mut() {
+                println!(
+                    "target x: {}, target y: {}",
+                    tf.translation.x + window.width() / 2.0 - sprite.size.x / 2.0,
+                    tf.translation.y + window.height() / 2.0 - sprite.size.y / 2.0
+                );
+                println!(
+                    "mouse  x: {}, mouse  y: {}",
+                    mouse_pos.x,
+                    window.height() - mouse_pos.y
+                );
+                println!(
+                    "window w: {}, window h: {}",
+                    window.width(),
+                    window.height(),
+                );
 
                 let t_x = tf.translation.x + window.width() / 2.0 - sprite.size.x / 2.0;
                 let t_y = tf.translation.y + window.height() / 2.0 - sprite.size.y / 2.0;
 
-                let m_x = mouse_pos.x;
-                let m_y = window.height() - mouse_pos.y;
-
-                if 
-                t_x < m_x &&
-                m_x < t_x + sprite.size.x &&
+                if t_x < mouse_pos.x &&
+                mouse_pos.x < t_x + sprite.size.x &&
 
                 // You have to do that with the mouse because y: 0 is at bottom
-                t_y < m_y &&
-                m_y < t_y + sprite.size.y
-                
+                t_y < mouse_pos.y &&
+                mouse_pos.y < t_y + sprite.size.y
                 {
                     println!("pressed on right position");
+                    commands.entity(entity).despawn();
+                    targets_on_screen.0 -= 1;
+                    gravity.0 += 0.1;
+
                 }
             }
         }
@@ -91,28 +98,47 @@ fn target_click(
 
 fn target_movement(
     gravity: Res<Gravity>,
-    mut query: Query<(&mut Transform, &mut Speed, With<Target>)>
+    mut query: Query<(&mut Transform, &mut Speed), With<Target>>,
+    time: Res<Time>,
 ) {
-    for (mut tf, mut acc, _) in query.iter_mut() {
+    for (mut tf, mut acc) in query.iter_mut() {
         acc.0 += gravity.0;
-        tf.translation.y -= acc.0;
+        tf.translation.y -= acc.0 * time.delta_seconds();
     }
+}
+
+fn target_reset(
+    mut query: Query<(Entity, &mut Transform), With<Target>>,
+    windows: Res<Windows>,
+    mut commands: Commands,
+    mut targets_on_screen: ResMut<TargetsOnScreen>,
+    mut gravity: ResMut<Gravity>,
+) {
+    let window = windows.get_primary().unwrap();
+    for (entity, tf) in query.iter_mut() {
+        if tf.translation.y < -window.height() / 2.0 {
+            commands.entity(entity).despawn();
+            targets_on_screen.0 -= 1;
+            gravity.0 = 1.0;
+        }
+    }
+
+
 }
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-
+        
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(TargetsOnScreen(0))
-        .insert_resource(Gravity(0.04))
+        .insert_resource(Gravity(1.0))
 
         .add_startup_system(setup.system())
-
         .add_system(spawn_target.system())
-        // .add_system(target_movement.system())
+        .add_system(target_movement.system())
         .add_system(target_click.system())
-        // .add_system(cursor_events.system())
-        
+        .add_system(target_reset.system())
+
         .run();
 }
