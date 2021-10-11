62434d02ddca344f125e37292a25b85937717a6b
dojje <dojje@users.noreply.github.com>
2021-10-11 22:46:26 +0200

    added functioning pausing state

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,13 @@
 //TODO Mouse should be a crosshair
 
 
-use bevy::prelude::*;
+use bevy::{ecs::query, prelude::*};
 use rand::Rng;
-
-struct TargetsOnScreen(u8);
 
 struct Target;
 
 struct Title;
+struct PausedScreenEntity;
 struct StartBtn;
 struct Speed(f32);
 struct Gravity(f32);
@@ -27,6 +26,7 @@ struct Score(u32);
 enum GameState {
     InGame,
     MainMenu,
+    Paused,
 }
 
 pub struct Materials {
@@ -37,47 +37,43 @@ pub struct Materials {
 
 fn setup(
     mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut color_material: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
 
     commands.insert_resource(Materials {
-        target: materials.add(asset_server.load("target.png").into()),
-        title: materials.add(asset_server.load("pocop.png").into()),
-        start_btn: materials.add(asset_server.load("start_btn.png").into()),
+        target: color_material.add(asset_server.load("target.png").into()),
+        title: color_material.add(asset_server.load("pocop.png").into()),
+        start_btn: color_material.add(asset_server.load("start_btn.png").into()),
     })
 }
 
 fn spawn_target(
-    mut targets_on_screen: ResMut<TargetsOnScreen>,
     mut commands: Commands,
     materials: Res<Materials>,
     windows: Res<Windows>,
 ) {
-    if targets_on_screen.0 <= 0 {
-        let mut rng = rand::thread_rng();
-        let window = windows.get_primary().unwrap();
+    let mut rng = rand::thread_rng();
+    let window = windows.get_primary().unwrap();
 
-        let target_width = (window.width() / 30.0 * window.height() / 30.0) / 8.0;
+    let target_width = (window.width() / 30.0 * window.height() / 30.0) / 8.0;
 
-        let target_x = rng.gen_range(
-            -window.width() / 2.0 + target_width / 2.0..window.width() / 2.0 - target_width / 2.0,
-        );
-        let target_y = window.height() / 2.0 - target_width / 2.0;
+    let target_x = rng.gen_range(
+        -window.width() / 2.0 + target_width / 2.0..window.width() / 2.0 - target_width / 2.0,
+    );
+    let target_y = window.height() / 2.0 - target_width / 2.0;
 
-        commands
-            .spawn_bundle(SpriteBundle {
-                material: materials.target.clone(),
-                transform: Transform::from_xyz(target_x, target_y, 0.0),
-                sprite: Sprite::new(Vec2::new(target_width, target_width)),
-                ..Default::default()
-            })
-            .insert(Target)
-            .insert(Speed(0.0));
-
-        targets_on_screen.0 += 1;
-    }
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.target.clone(),
+            transform: Transform::from_xyz(target_x, target_y, 0.0),
+            sprite: Sprite::new(Vec2::new(target_width, target_width)),
+            ..Default::default()
+        })
+        .insert(Target)
+        .insert(Speed(0.0));
 }
 
 fn target_click(
@@ -85,9 +81,10 @@ fn target_click(
     windows: Res<Windows>,
     mut query: Query<(&Transform, &Sprite, Entity), With<Target>>,
     mut commands: Commands,
-    mut targets_on_screen: ResMut<TargetsOnScreen>,
     mut gravity: ResMut<Gravity>,
     mut score: ResMut<Score>,
+    materials: Res<Materials>,
+    
 ) {
     if btn.just_pressed(MouseButton::Left) {
         let window = windows.get_primary().unwrap();
@@ -99,9 +96,25 @@ fn target_click(
 
                 if distance <= sprite.size.x / 2.0 {
                     commands.entity(entity).despawn();
-                    if targets_on_screen.0 > 0 {
-                        targets_on_screen.0 -= 1;
-                    }
+                    let mut rng = rand::thread_rng();
+                    let window = windows.get_primary().unwrap();
+                
+                    let target_width = (window.width() / 30.0 * window.height() / 30.0) / 8.0;
+                
+                    let target_x = rng.gen_range(
+                        -window.width() / 2.0 + target_width / 2.0..window.width() / 2.0 - target_width / 2.0,
+                    );
+                    let target_y = window.height() / 2.0 - target_width / 2.0;
+                
+                    commands
+                        .spawn_bundle(SpriteBundle {
+                            material: materials.target.clone(),
+                            transform: Transform::from_xyz(target_x, target_y, 0.0),
+                            sprite: Sprite::new(Vec2::new(target_width, target_width)),
+                            ..Default::default()
+                        })
+                        .insert(Target)
+                        .insert(Speed(0.0));
 
                     gravity.0 += 0.1;
                     score.0 += 1;
@@ -129,7 +142,6 @@ fn target_reset(
     mut query: Query<(Entity, &mut Transform), With<Target>>,
     windows: Res<Windows>,
     mut commands: Commands,
-    mut targets_on_screen: ResMut<TargetsOnScreen>,
     mut gravity: ResMut<Gravity>,
     mut score: ResMut<Score>,
 ) {
@@ -137,7 +149,6 @@ fn target_reset(
     for (entity, tf) in query.iter_mut() {
         if tf.translation.y < -window.height() / 2.0 {
             commands.entity(entity).despawn();
-            targets_on_screen.0 -= 1;
             gravity.0 = 1.0;
             println!("Score: {}", score.0);
             score.0 = 0;
@@ -145,9 +156,17 @@ fn target_reset(
     }
 }
 
-fn target_despawn(mut query: Query<Entity, With<Target>>, mut commands: Commands) {
+fn target_despawn(
+    mut query: Query<Entity,
+    With<Target>>,
+    mut commands: Commands,
+    mut gravity: ResMut<Gravity>,
+    mut score: ResMut<Score>,
+) {
     for entity in query.iter_mut() {
         commands.entity(entity).despawn();
+        gravity.0 = 1.0;
+        score.0 = 0;
     }
 }
 
@@ -169,10 +188,9 @@ fn switch_to_game(
 
 fn main_menu_setup(
     mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut color_material: ResMut<Assets<ColorMaterial>>,
     ui_materials: Res<Materials>,
 ) {
-    commands.spawn_bundle(UiCameraBundle::default());
 
     commands
         .spawn_bundle(NodeBundle {
@@ -183,10 +201,9 @@ fn main_menu_setup(
                 
                 ..Default::default()
             },
-            material: materials.add(Color::NONE.into()),
+            material: color_material.add(Color::NONE.into()),
             ..Default::default()
         })
-        
         .with_children(|parent| {
             // Title image
             parent.spawn_bundle(ImageBundle {
@@ -238,6 +255,68 @@ fn despawn_start_button(
     }
 }
 
+fn pausing(
+    kb: Res<Input<KeyCode>>,
+    mut game_state: ResMut<State<GameState>>,
+) {
+    if kb.just_pressed(KeyCode::Escape) {
+
+        match game_state.current() {
+            GameState::Paused => {
+                game_state.pop().unwrap();
+            }
+            _ => {
+                game_state.push(GameState::Paused).unwrap();
+
+            }
+        }
+    }
+}
+
+fn paused_setup(
+    mut commands: Commands,
+    mut color_material: ResMut<Assets<ColorMaterial>>,
+    ui_materials: Res<Materials>,
+) {
+
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::ColumnReverse,
+                
+                ..Default::default()
+            },
+            material: color_material.add(Color::NONE.into()),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            // Title image
+            parent.spawn_bundle(ImageBundle {
+                style: Style {
+                    size: Size::new(Val::Px(800.0), Val::Auto),
+                    margin: Rect { left: Val::Px(25.0), right: Val::Px(25.0), top: Val::Px(50.0), bottom: Val::Undefined },
+                    max_size: Size::new(Val::Percent(100.0), Val::Auto),
+                    min_size: Size::new(Val::Px(190.0), Val::Px(80.0)),
+
+                    ..Default::default()
+                },
+                material: ui_materials.title.clone(),
+                ..Default::default()
+            })
+            .insert(PausedScreenEntity);
+        }).insert(PausedScreenEntity);
+}
+
+fn paused_exit(
+    mut commands: Commands,
+    mut query: Query<Entity, With<PausedScreenEntity>>
+) {
+    for entity in query.iter_mut() {
+        commands.entity(entity).despawn();
+    }
+}
 
 fn main() {
     App::build()
@@ -247,7 +326,6 @@ fn main() {
         //
         // Resources
         .insert_resource(ClearColor(Color::rgb(0.927, 0.927, 0.927)))
-        .insert_resource(TargetsOnScreen(0))
         .insert_resource(Gravity(1.0))
         .insert_resource(Score(0))
         //
@@ -257,29 +335,64 @@ fn main() {
         // Startup systems
         .add_startup_system(setup.system())
         //
+        // Pausing
+        .add_system(pausing.system())
+        //
         // System Sets
         //
         // Main Menu set
         .add_system_set(
-            SystemSet::on_enter(GameState::MainMenu).with_system(main_menu_setup.system()),
+            SystemSet::on_enter(GameState::MainMenu)
+            .with_system(main_menu_setup.system()),
         )
         .add_system_set(
-            SystemSet::on_update(GameState::MainMenu).with_system(switch_to_game.system()),
+            SystemSet::on_update(GameState::MainMenu)
+            .with_system(switch_to_game.system()),
         )
         .add_system_set(
             SystemSet::on_exit(GameState::MainMenu)
             .with_system(despawn_title.system())
             .with_system(despawn_start_button.system())
         )
+        // Paused
+        .add_system_set(
+            SystemSet::on_enter(GameState::Paused)
+            .with_system(paused_setup.system())
+
+        )
+        .add_system_set(
+            SystemSet::on_exit(GameState::Paused)
+            .with_system(paused_exit.system())
+
+        )
+        .add_system_set(
+            SystemSet::on_update(GameState::Paused)
+                
+        )
         // InGame set
         .add_system_set(
+            SystemSet::on_enter(GameState::InGame)
+            .with_system(spawn_target.system())
+        )
+        
+        .add_system_set(
             SystemSet::on_update(GameState::InGame)
-                .with_system(spawn_target.system())
                 .with_system(target_movement.system())
                 .with_system(target_click.system())
                 .with_system(target_reset.system()),
         )
-        .add_system_set(SystemSet::on_exit(GameState::InGame).with_system(target_despawn.system()))
+        .add_system_set(
+            SystemSet::on_pause(GameState::InGame)
+                .with_system(target_despawn.system())
+        )
+        .add_system_set(
+            SystemSet::on_resume(GameState::InGame)
+                .with_system(spawn_target.system())
+        )
+        .add_system_set(
+            SystemSet::on_exit(GameState::InGame)
+            .with_system(target_despawn.system())
+        )
         //
         // Running it
         .run();
